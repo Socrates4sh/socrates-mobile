b5fdc20b4de2a97cf1a2d51f231e9cdff9004885
Kirti <socratesflutterflow@gmail.com>
2025-07-18 15:43:24 +0530

    push

--- lib/video_page_v2/video_page_v2_widget.dart
+++ lib/video_page_v2/video_page_v2_widget.dart
@@ -1,3 +1,5 @@
+import '../auth/firebase_auth/auth_util.dart';
+import '../components/locked_widget.dart';
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
