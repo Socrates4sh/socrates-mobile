0ee5a52705ee042710d12762f45631a747bda7ff
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-07-18 09:25:23 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/chapters/chapters_widget.dart
+++ lib/pages/chapters/chapters_widget.dart
@@ -6,12 +6,9 @@ import '/flutter_flow/flutter_flow_util.dart';
 import '/pages/payment/payment_box/payment_box_widget.dart';
 import '/flutter_flow/revenue_cat_util.dart' as revenue_cat;
 import '/index.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 import 'chapters_model.dart';
 export 'chapters_model.dart';
 
